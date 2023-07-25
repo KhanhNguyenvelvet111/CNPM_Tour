47b7185c9395728f42a6f127eb80f17d3ad62429
USER <USER@LAPTOP-JOA7S7CH>
2023-07-26 01:52:19 +0700

    Add project files.

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CNPM_Tour.Controllers
+{
+    public class AdminController : Controller
+    {
+        
+        public ActionResult TrangChu()
+        {
+
+            return View();
+        }
+        public ActionResult QLTour()
+        {
+
+            return View();
+        }
+        public ActionResult QLNhanVien()
+        {
+
+            return View();
+        }
+        public ActionResult QLKhachHang()
+        {
+
+            return View();
+        }
+        public ActionResult ChuongTrinhKhuyenMai()
+        {
+
+            return View();
+        }
+        public ActionResult QLDoiTac()
+        {
+
+            return View();
+        }
+        public ActionResult BaoCaoTaiChinh()
+        {
+
+            return View();
+        }
+        public ActionResult HoTroKH()
+        {
+
+            return View();
+        }
+        public ActionResult CTTour()
+        {
+
+            return View();
+        }
+    }
+}
